5db844342e4d2474c7d547fb9700b69727f087f4
Maciej Preus <mpreus@onet.eu>
2019-12-15 22:16:18 +0100

    Add desktop component in app.js file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
+/* komponent nawigacji desktopowej - w poziomie */
+class DesktopMenu extends React.Component {
+    state = {
+        isOpen: true
+    }
+
+    openMenu = (e) => {
+        e.preventDefault()
+        this.setState({
+            isOpen: false
+        })
+    }
+
+    closeMenu = (e) => {
+        e.preventDefault()
+        this.setState({
+            isOpen: true
+        })
+    }
+
+    render() {
+        return (
+            <section className="desktop">
+                <div className="desktop-menu">
+                    <a href="#" onClick={this.openMenu} className="open-menu"></a>
+                    <nav className={this.state.isOpen ? "" : "hide"}>
+                        <ul>
+                            <li><a href="#">O nas</a></li>
+                            <li><a href="#">Zespół</a></li>
+                            <li><a href="#">Produkty</a></li>
+                            <li><a href="#">Kontakt</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </section>
+        )
+    }
+}
+/* komponent nawigacji mobilnej - w pionie */
 class MobileMenu extends React.Component {
     state = {
         isOpen: false
@@ -16,7 +55,7 @@ class MobileMenu extends React.Component {
             isOpen: false
         })
     }
-/* prosta struktura nawigacji */
+
     render() {
         return (
             <section className="mobile">
@@ -36,9 +75,10 @@ class MobileMenu extends React.Component {
         )
     }
 }
-
+/* warunkowe renderowanie nawigacji w zależności od szerokości ekranu */
 function App() {
-    return <MobileMenu />
+    let screenWidth = screen.width;
+    return screenWidth > 800 ? <DesktopMenu /> : <MobileMenu />
 }
 
 ReactDOM.render(
